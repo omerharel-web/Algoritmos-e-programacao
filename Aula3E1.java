4365cb70598d9bfc8f62f7c1d22e3b69d27a2494
omerharel-web <omerharrrel@gmail.com>
2026-09-02 09:46:35 -0300

    Add files via upload

---
+++ Aula3E1.java
@@ -0,0 +1,8 @@
+public class Aula3E1 {
+    public static void main(String[] args) {
+
+        System.out.println("Omer Harel");
+        System.out.println("Registro Academico:06345137");
+        System.out.println("SUPERIOR DE TECNOLOGIA EM ANALISE E DESENVOLVIMENTO DE SISTEMAS");
+    }
+}
---
+++ Aula3E2.java
@@ -0,0 +1,8 @@
+public class Aula3E2 {
+public static void main(String[] args) {
+
+    System.out.println("Minha idade e 26");
+    System.out.println("Altura 1,75M");
+    System.out.println("presente");
+}
+}
